f4fffc4203ee91ca17a770f98538c9b8446f0129
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-04-23 17:36:51 +0800

    eureka 集群

---
+++ cloud-eureka-7003/src/main/java/pres/hjc/cloud/Eureka7003Application.java
@@ -0,0 +1,22 @@
+package pres.hjc.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author HJC
+ * @version 1.0
+ * To change this template use File | Settings | File Templates.
+ * @date 2020/4/23
+ * @time 17:33
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Eureka7003Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Eureka7003Application.class,args);
+    }
+}
